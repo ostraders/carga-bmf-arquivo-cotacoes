e29b466a781cfcaccdb8a2b35e06cb983f20f1db
João Rodrigues <ricardo.cocati@gmail.com>
2018-07-08 01:33:22 -0300

    Correção da verificação de existência de arquivo

--- carga-bmf-arquivo-cotacoes-service/src/main/java/com/ricardococati/diretorios/GerenciadorArquivos.java
+++ carga-bmf-arquivo-cotacoes-service/src/main/java/com/ricardococati/diretorios/GerenciadorArquivos.java
@@ -1,5 +1,7 @@
 package com.ricardococati.diretorios;
 
+import static java.util.Objects.nonNull;
+
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
@@ -15,69 +17,76 @@ import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 @Service
-public class GerenciadorArquivos implements IGerenciadorArquivos, Serializable{
+public class GerenciadorArquivos implements IGerenciadorArquivos, Serializable {
 
-	private static final long serialVersionUID = -1283658257967124514L;
-	private static final String LOCK = "@.lock";
-	private SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy hh:mm:ss");
+  private static final long serialVersionUID = -1283658257967124514L;
+  private static final String LOCK = "@.lock";
+  private SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy hh:mm:ss");
 
-	@Override
-	public void moverArquivosEntreDiretorios(String origem, String destino) throws Exception, FileNotFoundException, IOException{
-		File arquivosDiretorioOrigem = new File(origem);
-		File diretorioDestino = new File(destino);
-		File arrayArquivos[] = arquivosDiretorioOrigem.listFiles();
-		File arquivo;
-		int i = 0;
-		if (arrayArquivos.length > 0) {
-			for (int j = arrayArquivos.length; i < j; i++) {
-				arquivo = arrayArquivos[i];
-				moveArquivoParaDiretorio(diretorioDestino, arquivo);
-			}
-		}
-	}
-	
-	@Override
-	public void moverArquivosEntreDiretoriosVerificaDiretorio(String origem, String destino) throws Exception, FileNotFoundException, IOException{
-		File arquivosDiretorioOrigem = new File(origem);
-		File diretorioDestino = new File(destino);
-		File arrayArquivos[] = arquivosDiretorioOrigem.listFiles();
-		File arquivo;
-		int i = 0;
-		if (arrayArquivos.length > 0) {
-			for (int j = arrayArquivos.length; i < j; i++) {
-				arquivo = arrayArquivos[i];
-				moveArquivoParaDiretorio(diretorioDestino, arquivo);
-			}
-		} else {
-        	log.info("Não existem arquivos no diretório!");
-        }
-	}
+  @Override
+  public void moverArquivosEntreDiretorios(String origem, String destino)
+      throws Exception, FileNotFoundException, IOException {
+    File arquivosDiretorioOrigem = new File(origem);
+    File diretorioDestino = new File(destino);
+    File arrayArquivos[] = arquivosDiretorioOrigem.listFiles();
+    File arquivo;
+    int i = 0;
+    if (arrayArquivos.length > 0) {
+      for (int j = arrayArquivos.length; i < j; i++) {
+        arquivo = arrayArquivos[i];
+        moveArquivoParaDiretorio(diretorioDestino, arquivo);
+      }
+    }
+  }
 
-	@Override
-	public void moveArquivoParaDiretorio(File diretorioDestino, File arquivo) throws Exception, FileNotFoundException, IOException {
-		arquivo.renameTo(new File(diretorioDestino, arquivo.getName()));
-		log.info("Arquivo movido para o diretório - " + diretorioDestino.getPath() + "/" + arquivo.getName() + " às " + sdf.format(new Date()));
-	}
-	
-	@Override
-	public File renomearArquivo(File arquivo) {
-		arquivo.renameTo(new File(CaminhoArquivoEnum.CAMINHO_ARQUIVO_ENTRADA.getCaminho(), arquivo.getName()+ LOCK));
-		arquivo = new File(CaminhoArquivoEnum.CAMINHO_ARQUIVO_ENTRADA.getCaminho(), arquivo.getName()+ LOCK);
-		return arquivo;
-	}
+  @Override
+  public void moverArquivosEntreDiretoriosVerificaDiretorio(String origem, String destino)
+      throws Exception, FileNotFoundException, IOException {
+    File arquivosDiretorioOrigem = new File(origem);
+    File diretorioDestino = new File(destino);
+    File arrayArquivos[] = arquivosDiretorioOrigem.listFiles();
+    File arquivo;
+    int i = 0;
+    if (nonNull(arrayArquivos) && arrayArquivos.length > 0) {
+      for (int j = arrayArquivos.length; i < j; i++) {
+        arquivo = arrayArquivos[i];
+        moveArquivoParaDiretorio(diretorioDestino, arquivo);
+      }
+    } else {
+      log.info("Não existem arquivos no diretório!");
+    }
+  }
 
-	@Override
-	public boolean validarArquivoLock(File arquivo) {
-		boolean retorno = false;
-		if(arquivo.getName().contains(LOCK)){
-			retorno = true;
-		}
-		return retorno;
-	}
+  @Override
+  public void moveArquivoParaDiretorio(File diretorioDestino, File arquivo)
+      throws Exception, FileNotFoundException, IOException {
+    arquivo.renameTo(new File(diretorioDestino, arquivo.getName()));
+    log.info(
+        "Arquivo movido para o diretório - " + diretorioDestino.getPath() + "/" + arquivo.getName()
+            + " às " + sdf.format(new Date()));
+  }
 
-	@Override
-	public void removeLock(File diretorioDestino, File arquivo) {
-		arquivo.renameTo(new File(diretorioDestino, arquivo.getName().replaceAll(LOCK, "")));
-	}
+  @Override
+  public File renomearArquivo(File arquivo) {
+    arquivo.renameTo(new File(CaminhoArquivoEnum.CAMINHO_ARQUIVO_ENTRADA.getCaminho(),
+        arquivo.getName() + LOCK));
+    arquivo = new File(CaminhoArquivoEnum.CAMINHO_ARQUIVO_ENTRADA.getCaminho(),
+        arquivo.getName() + LOCK);
+    return arquivo;
+  }
+
+  @Override
+  public boolean validarArquivoLock(File arquivo) {
+    boolean retorno = false;
+    if (arquivo.getName().contains(LOCK)) {
+      retorno = true;
+    }
+    return retorno;
+  }
+
+  @Override
+  public void removeLock(File diretorioDestino, File arquivo) {
+    arquivo.renameTo(new File(diretorioDestino, arquivo.getName().replaceAll(LOCK, "")));
+  }
 
 }
