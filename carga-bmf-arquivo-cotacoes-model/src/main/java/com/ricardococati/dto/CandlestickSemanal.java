d14d2b59008ea7cee2f21e980ae0bae69abe4945
João Rodrigues <ricardo.cocati@gmail.com>
2018-07-16 13:14:13 -0300

    Criação das classes de Candlestick

---
+++ carga-bmf-arquivo-cotacoes-model/src/main/java/com/ricardococati/dto/CandlestickSemanal.java
@@ -0,0 +1,30 @@
+package com.ricardococati.dto;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import lombok.Builder;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Data
+@Document(collection = "candlestick_semanal")
+@EqualsAndHashCode(callSuper = false)
+@NoArgsConstructor
+public class CandlestickSemanal extends BMFCargaDTO {
+
+  private static final long serialVersionUID = 505011356059052924L;
+  @Id
+  private String id;
+  private LocalDate dtpreg;
+  private Long idsemana;
+  private String nomres;
+  private BigDecimal preabe;
+  private BigDecimal premax;
+  private BigDecimal premin;
+  private BigDecimal preult;
+  private BigDecimal voltot;
+
+}
