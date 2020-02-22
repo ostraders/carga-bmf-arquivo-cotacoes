ae14ebf58a1f0b35ff64469b664cc99a0edcd137
ricardococati <ricardo.cocati@ifood.com.br>
2020-02-22 13:43:45 -0300

    Mudando imports dos testes

--- model/src/main/java/com/ricardococati/model/dto/CotacaoDTO.java
+++ model/src/main/java/com/ricardococati/model/dto/CotacaoDTO.java
@@ -5,8 +5,10 @@ import java.time.LocalDate;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 
+@EqualsAndHashCode(callSuper = true)
 @Data
 @Builder
 @AllArgsConstructor
