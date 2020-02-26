0a0e1671f40255346258a28f25129da748e5cbf4
ricardococati <ricardo.cocati@ifood.com.br>
2020-02-26 12:11:01 -0300

    Corrigindo volumes dos bancos do postgres docker

--- monta_ambiente.sh
+++ monta_ambiente.sh
@@ -17,41 +17,41 @@ else
 	exit 1
 fi
 
-sudo docker ps --format "{{.Names}}" | grep 'ostraders' &> /dev/null
+docker ps --format "{{.Names}}" | grep 'carga-bmf-arquivo-cotacoes' &> /dev/null
 
 if [ $? == 0 ]; then
    echo "Containers já existem no sistema operacional"
    sudo chmod 777 /var/run/docker.sock
-   sudo docker start --detach-keys --interactive carga-bmf-arquivo-cotacoes_kafka_1
-   sudo docker start --detach-keys --interactive carga-bmf-arquivo-cotacoes_pg-dbbmf_1
-   sudo docker start --detach-keys --interactive carga-bmf-arquivo-cotacoes_pg-calcula-dbbmf_1
+   docker start --detach-keys --interactive carga-bmf-arquivo-cotacoes_kafka_1
+   docker start --detach-keys --interactive carga-bmf-arquivo-cotacoes_pg-dbbmf_1
+   docker start --detach-keys --interactive carga-bmf-arquivo-cotacoes_pg-calcula-dbbmf_1
 else
 	echo "PARANDO TODAS MAQUINAS DOCKER"
-	sudo docker stop $(docker ps -aq)
+	docker stop $(docker ps -aq)
 
 	echo "AGUARDA 2 SEGUNDOS"
 	sleep 2
 
 	echo "REMOVENDO TODAS MAQUINAS DOCKER"
-	sudo docker rm -f $(docker ps -aq)
+	docker rm -f $(docker ps -aq)
 
 	echo "AGUARDA 2 SEGUNDOS"
 	sleep 2
 
 	echo "CRIANDO MÁQUINAS DOCKER(KAFKA E POSTGRESQL)"
-	sudo docker-compose up -d
+	docker-compose up -d
 
 	echo "AGUARDA 5 SEGUNDOS"
 	sleep 5
 
 	echo "CRIANDO TÓPICOS NO KAFKA DIARIO"
-	sudo docker exec ostraders_kafka_1 /opt/kafka_2.11-0.10.1.0/bin/kafka-topics.sh --create --zookeeper localhost:2181 --replication-factor 1 --partitions 1 --topic candlestick-diario
+	docker exec carga-bmf-arquivo-cotacoes_kafka_1 /opt/kafka_2.11-0.10.1.0/bin/kafka-topics.sh --create --zookeeper localhost:2181 --replication-factor 1 --partitions 1 --topic candlestick-diario
 
 	echo "AGUARDA 5 SEGUNDOS"
 	sleep 5
 
 	echo "CRIANDO TÓPICOS NO KAFKA SEMANAL"
-	sudo docker exec ostraders_kafka_1 /opt/kafka_2.11-0.10.1.0/bin/kafka-topics.sh --create --zookeeper localhost:2181 --replication-factor 1 --partitions 1 --topic candlestick-semanal
+	docker exec carga-bmf-arquivo-cotacoes_kafka_1 /opt/kafka_2.11-0.10.1.0/bin/kafka-topics.sh --create --zookeeper localhost:2181 --replication-factor 1 --partitions 1 --topic candlestick-semanal
 
   exit 1
 fi
